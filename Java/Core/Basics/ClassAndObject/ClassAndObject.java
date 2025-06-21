893e9da90c937926d31b917a0b4dfb9299fea17d
AAYUSH KUKADE <akukade10@gmail.com>
2025-06-21 15:55:24 +0530

    updated the comments for better understanding

--- Java/Core/Basics/ClassAndObject/ClassAndObject.java
+++ Java/Core/Basics/ClassAndObject/ClassAndObject.java
@@ -1,3 +1,11 @@
+/**
+ * Performs Various functions and demonstrates various functions of objects.
+ * 
+ * @param obj       add(), multiply().
+ * @param num1,num2 holds numerical values.
+ * @print obj.
+ */
+
 class Calculator {
     public int add(int a, int b) {
         int r = a + b;
@@ -21,3 +29,9 @@ public class ClassAndObject {
         System.out.println(result);
     }
 }
+
+/*
+ * this demostrates the Object and its pracitcal use : we can create as many
+ * object as we want basically an object or obj is bascially an instance of that
+ * class : the instance or obj inherits the class traits
+ */
